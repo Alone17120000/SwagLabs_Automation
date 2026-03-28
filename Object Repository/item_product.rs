b07edf22e48edffdfd17f92ff1e922ae0ef17a64
quanghunh <nhatquangh72@gmail.com>
2026-03-29 00:03:55 +0700

    add:OR(item_product)

---
+++ Object Repository/item_product.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>item_product</name>
+   <tag></tag>
+   <elementGuidId>61297128-0e61-49b8-9976-fbc6522b5848</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='inventory_item']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
