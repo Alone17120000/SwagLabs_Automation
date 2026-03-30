c0620a4553f6fbb5dbdc651ee585d3de530e7fa7
KhoaPR1991 <147786955+KhoaPR1991@users.noreply.github.com>
2026-03-30 21:11:04 +0700

    TC 23

---
+++ Scripts/CheckoutAndLogout/TC23/Script1774879744089.groovy
@@ -0,0 +1,19 @@
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+
+WebUI.openBrowser('https://www.saucedemo.com/')
+WebUI.setText(findTestObject('Object Repository/txt_Username'), 'standard_user')
+WebUI.setText(findTestObject('Object Repository/txt_Password'), 'secret_sauce')
+WebUI.click(findTestObject('Object Repository/btn_Login'))
+
+WebUI.click(findTestObject('Object Repository/btn_AddToCart'))
+WebUI.navigateToUrl('https://www.saucedemo.com/cart.html')
+
+WebUI.delay(1)
+
+WebUI.click(findTestObject('Object Repository/btn_RemoveInCart'))
+
+WebUI.verifyElementNotPresent(findTestObject('Object Repository/item_Cart_Product'), 3)
+
+WebUI.closeBrowser()
