58899840c117e4b200057a16a77a30d2a75a1fdc
KhoaPR1991 <147786955+KhoaPR1991@users.noreply.github.com>
2026-03-31 00:03:01 +0700

    bổ sung Tc 28

---
+++ Object Repository/btn_AddOnesie.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_AddOnesie</name>
+   <tag></tag>
+   <elementGuidId>92f9f020-5564-4aa1-a79a-daedb32249d2</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@data-test='add-to-cart-sauce-labs-onesie']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
